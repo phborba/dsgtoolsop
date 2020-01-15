7eb6fb40dae18cbcf08a699c358bc998577253fb
Joao Felipe Aguiar Guimaraes <jf.aguiar@hotmail.com>
2020-01-15 19:47:05 -0300

    Updated init.py
    Updated init.py
    Renamed dsgToolsOp.py to dsgtoolsop.py

--- __init__.py
+++ __init__.py
@@ -32,5 +32,5 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :type iface: QgsInterface
     """
     #
-    from .dsgToolsOp import DsgToolsOp
+    from .dsgtoolsop import DsgToolsOp
     return DsgToolsOp(iface)
